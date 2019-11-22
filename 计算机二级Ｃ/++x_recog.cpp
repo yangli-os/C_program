a5a5fcfa81fe57ad7675a9b28a4f49610510448f
16647615268 <47737324+16647615268@users.noreply.github.com>
2019-11-22 14:40:13 +0800

    Add files via upload

---
+++ 计算机二级Ｃ/++x_recog.cpp
@@ -0,0 +1,22 @@
+﻿#include "stdafx.h"
+#include<ios.stream>
+using namespace std；
+
+int main()
+{
+	for (int i = 1; i <= 100; i++)
+	{
+		x = i;
+		if (++x % 2 == 0)
+		{
+			cout << x << ",";
+			if (++x % 3 == 0)
+			{
+				cout << x << ",";
+				if (++x % 7 == 0)
+					cout << x << ",";
+			}
+		}
+	}
+	return 0;
+}
---
+++ 计算机二级Ｃ/i_j_k_printf.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+
+int main()
+{
+	int a = 10, b, c, d, i, j, k;
+	b = c = d = 5;
+	i = j = k = 0;
+	for (; a > b; ++b)
+		i++;
+	while (a > ++c)
+		j++;
+	do {
+		k++;
+	} while (a > d++);
+	std::cout << i << "," << j << "," << k;
+	return 0;
+}
