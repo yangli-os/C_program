37547de11a3ba5b61261f36f5472419b37dc59e6
YangLi <47737324+16647615268@users.noreply.github.com>
2020-05-28 17:10:12 +0800

    Quicksort_C++_float_Stack or Queue
    使用栈进行快速排序，输入为float类型的数据，但是不能转换成C语言程序。
    uses a stack for quicksort, enters data of type float, but cannot be converted to a C program.

---
+++ Matlab函数替换/QuickSort_C++.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<queue>
+//使用栈实现快速排序
+using namespace std;
+ 
+void QSort(float* array, float count) {
+	queue<float> First;                    //队列
+	queue<float> Last;
+	First.push(0);
+	Last.push(count - 1);
+	while(!First.empty()) {
+		int first = First.front();
+		int last = Last.front();
+		First.pop(); Last.pop();
+		float ctrl = array[first];
+		float curr = 0;
+		int i = first, j = last;
+		while(i < j) {
+			if(curr == 0) {
+				while(array[j] > ctrl && j != i) j--;
+				array[i] = array[j];
+				curr = 1;
+			} else {
+				while(array[i] <= ctrl && j != i) i++;
+				array[j] = array[i];
+				curr = 0;
+			}
+		}
+		array[i] = ctrl;
+		if(first < i - 1) {
+			First.push(first);
+			Last.push(i - 1);
+		}
+		if(last > i + 1) {
+			First.push(i + 1);
+			Last.push(last);
+		}
+	}
+}
+
+void PrintArray(float* a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("%f ", a[i]);
+	}
+	printf("\n");
+}
+int main() {
+	float test[10] = { 4.2,1.2,7.3,9.0,3.0,2.0,6.0,5.0,0.0,8.0 };
+	QSort(test, 10);
+	PrintArray(test,10);
+	return 0;
+}
