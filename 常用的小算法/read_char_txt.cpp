752ea1813a945f13cb1a9af26ad0197ef0f4bb23
YangLi <47737324+yangli-os@users.noreply.github.com>
2022-08-30 13:55:31 +0800

    Update read_char_txt.cpp

--- 常用的小算法/read_char_txt.cpp
+++ 常用的小算法/read_char_txt.cpp
@@ -2,25 +2,29 @@
 #include<vector>
 #include<fstream>
 #include<typeinfo>
+#include<string.h>
 
 using namespace std;
 
 int main()
 {
-	char buf[1024];                //临时保存读取出来的文件内容
-	string message;
-	ifstream infile;
-	infile.open("../data.txt");
-	if (infile.is_open())          //文件打开成功,说明曾经写入过东西
-	{
-		while (infile.good() && !infile.eof())
-		{
-			memset(buf, 0, 1024);
-			infile.getline(buf, 1204);
-			message = buf;
-                   //这里可能对message做一些操作
-				cout << message << endl;
-		}
-		infile.close();
-	}
+ char buf[1024];                //临时保存读取出来的文件内容
+ char filePath[1024];
+ char fileName[1024];
+ int len_str;
+ string message;
+ ifstream infile;
+ infile.open("C://Users//dobi//Desktop//name_list.txt");
+ if (infile.is_open())          //文件打开成功,说明曾经写入过东西
+ {
+  while (infile.good() && !infile.eof())
+  {
+   memset(buf, 0, 1024);
+   infile.getline(buf, 1024);
+   len_str = strlen(buf);
+   strncpy(filePath, buf, len_str-25);
+   strncpy(fileName, buf, len_str);
+  }
+  infile.close();
+ }
 }
